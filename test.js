0ac76cc78746c1b909360034780318c9bced1cad
Shaan Bhalaru <sbhalaru@gmail.com>
2024-04-10 22:48:57 -0700

    no big Changes

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
-console.log("test");
+const p = document.querySelector('.evenList');
+
+let evenList = [];
+let html = '';
+
+let loopprint = function(){
+    for (let i = 0; i < 10; i++) {
+        if (i % 2 == 0) {
+            html += `<li style="color: red">${i}</li>`;
+        };
+    };
+};
+
+console.log("test")
+loopprint();
+p.innerHTML = html;
